15b015db9ceb1298ede982b0b3623a6423fda037
Asad Sayeed <asayeed@mbl.ca>
2023-06-07 03:23:58 +0200

    worked example

--- src/lmprobs.py
+++ src/lmprobs.py
@@ -1,12 +1,14 @@
 import os
 import sys
-from sklearn.neighbours import KDTree
+from sklearn.neighbors import KDTree
 from skimage.transform import resize
 from nltk.util import trigrams
 from nltk.lm import MLE
 import numpy as np
 from operator import itemgetter
 from nltk.lm.preprocessing import padded_everygram_pipeline
+import pickle
+
 
 class AbstractSurprisalSpace:
     def __init__(self, dims):
@@ -15,15 +17,15 @@ class AbstractSurprisalSpace:
     def fit(self, sequences):
         self.train(sequences)
 
-        self.suprisalvecs = [self.surprisalizer_(x) for x in sequences]
+        self.surprisalvecs = [self.surprisalizer_(x) for x in sequences]
         self.nnfinder = KDTree(self.surprisalvecs)
 
-    def find(self, vec_index, k=5):
-        indices = self.nnfinder.query(self.surprisalvecs[vec_index].reshape(1,-1),
+    def find_index(self, vec_index, k=5):
+        dists, indices = self.nnfinder.query(self.surprisalvecs[vec_index].reshape(1,-1),
                                       k=k)
-        return indices, itemgetter(*vec_index)(self.surprisalvecs)
+
+        return list(dists[0]), list(indices[0]), itemgetter(*list(indices[0]))(self.surprisalvecs)
     
-        
 class TrigramSurprisalSpace(AbstractSurprisalSpace):
     def __init__(self, dims):
         super(TrigramSurprisalSpace, self).__init__(dims)
@@ -35,7 +37,37 @@ class TrigramSurprisalSpace(AbstractSurprisalSpace):
         self.lm.fit(trainingdata, vocab)
 
     def surprisalizer_(self, sentence):
-        trisent = trigrams(x)
-        return resize(np.array([-self.lm.logscore(x[2], [x[0], x[1]]) for x in trisent]), self.dims)
+        trisent = list(trigrams(sentence))
+        surps = np.array([-self.lm.logscore(x[2], [x[0], x[1]]) for x in trisent])
+        resized = resize(surps, (self.dims,))
+        return resized
 
 
+if __name__ == "__main__":
+    tss = TrigramSurprisalSpace(7)
+    itemfile = open("../babylm_10M_tokens.txt", "r")
+    tokens = [x[:-1].split(",") for x in itemfile]
+    #print(tokens[:5000])
+    tss.fit(tokens[:5000])
+
+    distances, indices, vectors = tss.find_index(3999)
+
+    print("We get distances {} at indices {}.\nThe vectors are:\n{}".format(distances, indices, vectors))
+    
+    distances, indices, vectors = tss.find_index(2124)
+
+    print("We get distances {} at indices {}.\nThe vectors are:\n{}".format(distances, indices, vectors))
+    
+    pickle.dump(tss, open("tss.pkl", "wb"))
+
+    loadtss = pickle.load(open("tss.pkl", "rb"))
+
+    distances, indices, vectors = loadtss.find_index(2124)
+
+    print("We get distances {} at indices {}.\nThe vectors are:\n{}".format(distances, indices, vectors))
+
+    distances, indices, vectors = loadtss.find_index(1111)
+
+    print("We get distances {} at indices {}.\nThe vectors are:\n{}".format(distances, indices, vectors))
+
+    
